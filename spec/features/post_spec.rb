c6669e6fc2f73680fc20e98301d16f052236b3a3
jerson1207 <jersonjabatstudent@gmail.com>
2021-09-04 11:32:38 +0800

    refactor post

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -14,13 +14,14 @@ describe 'navigate' do
   end
 
   describe 'create' do
-    it 'has a new form that can be reached' do
+    before do
       vasit new_post_path
+    end
+    it 'has a new form that can be reached' do
       expect(page.status_code).to eq(200)
     end
 
     it 'can be created from new form page' do
-      visit new_post_path
       fill_in 'post[date]', with: Date.today
       fill_in 'post[body]', with: "text here"
       click_on "Save"
