60a496789483827c16bb1237f1c58b6e8aaeff06
jerson1207 <jersonjabatstudent@gmail.com>
2021-09-04 10:46:37 +0800

    generate Post

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
   root to: 'page#homepage'
   devise_for :users
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Creation" do
+    before do
+      @user = User.create(email: "user@test.com", password: "123456", password_confirmation: "12356")
+    end
+    it "can create user" do
+      expect(@user).to be_valid
+    end
+
+    it "cannot create without first_name, last_name" do
+      @user.first_name = nil
+      @user.last_name = nil
+      expect(@user).to_root be_valid
+    end
+  end
 end
+
