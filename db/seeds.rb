0cb1e934e796e96311b2200fd51a92a1a6a94813
jerson1207 <jersonjabatstudent@gmail.com>
2021-09-05 03:54:44 +0800

    add create

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,17 @@
-@user = User.create(email: "user2@test.com", password: "123456", password_confirmation: "123456", first_name: "Juan", last_name: "Tester")
+@user1 = User.create(email: "user1@test.com", password: "123456", password_confirmation: "123456", first_name: "Juan", last_name: "Tester")
 puts "user2@test.com created"
 
+10.times do |post|
+  Post.create!(date: Date.today, body: "#{post} body content", user_id: @user1.id)
+end
+puts "10 posts created for user Juan"
+
+@user2 = User.create(email: "user2@test.com", password: "123456", password_confirmation: "123456", first_name: "Pedro", last_name: "Tester")
+puts "user3@test.com created"
 
 10.times do |post|
-  Post.create!(date: Date.today, body: "#{post} body content", user_id: @user.id)
+  Post.create!(date: Date.today, body: "#{post} body content", user_id: @user2.id)
 end
-puts "10 posts created"
+puts "10 posts created for user Pedro"
+
+
--- spec/features/static_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-describe "navigate", :type => :feature do
-
-end
