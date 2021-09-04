0f2c32cf9a86dbf5a9d4f371a068eec3d9bda196
jerson1207 <jersonjabatstudent@gmail.com>
2021-09-05 02:01:50 +0800

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
-10.times do |post|
-  Post.create!(date: Date.today, body: "#{post}body content")
-end
+@user = User.create(email: "user2@test.com", password: "123456", password_confirmation: "123456", first_name: "Juan", last_name: "Tester")
+puts "user2@test.com created"
 
-puts "10 Posts have been created"
+
+10.times do |post|
+  Post.create!(date: Date.today, body: "#{post} body content", user_id: @user.id)
+end
+puts "10 posts created"
